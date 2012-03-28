6703585c93687c9b33697569478275b4887e3262
Alan Leard <aleard@appcelerator.com>
2012-03-27 17:08:25 -0700

    Fixed directory structure

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,47 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+/**
+ * Scaffolding App
+ * @version      1.0
+ */
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+Ti.UI.iPhone.StatusBar.TRANSLUCENT_BLACK;
 
+// Include global objects
+Ti.include(
+    // App level files
+    'lib/app.js',
+    'lib/api.js',
+    'plugins/joli.js',
+    // Models
+    'models/messages.js',
+    // UI
+    'ui/dashboard/dashboard.js',
+    'ui/dashboard/style.js',
+    // Controllers
+    'controllers/dashboard.js'
+);
 
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
+/** This should kick off any platform **/
+// Setup the database dependencies
+App.API.setupDatabase();
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+/* 
+ * Things like global object assignment / module use could go here
+ * Can set things like App.platform, App.type here, etc.
+ */
 
-win1.add(label1);
+// Open the app window (This is a one window app sample)
+App.Window.open();
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
+/**
+ * TEST
+ */
+App.currentScreen = new App.Controllers.Dashboard();
+App.currentScreen.open();
+/**
+ * END TEST
+ */
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+// Global system Events
+Ti.Gesture.addEventListener('orientationchange', App.orientationObserverUpdate);
+Ti.Network.addEventListener('change', App.networkObserverUpdate);
+Ti.App.addEventListener('resume', App.resumeObserverUpdate);
